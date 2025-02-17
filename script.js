6708a6e6332cadbfe093cae04fd2a91ed6d2a1b1
Li Hua <lihua@email.com>
2025-02-17 15:05:38 +0800

    添加彩蛋
    添加彩蛋，完成 https://github.com/37tt/tang-love/issues/9

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let yesButton = document.getElementById("yes");
 let noButton = document.getElementById("no");
+let eggButton = document.getElementById("egg");
 let questionText = document.getElementById("question");
 let mainImage = document.getElementById("mainImage");
 
@@ -61,7 +62,7 @@ const loveTest = `!!!喜欢你!! ( >᎑<)♡︎ᐝ  ${
   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
 }`;
 
-yesButton.addEventListener("click", function () {
+function ok() {
   // 先创建基础 HTML 结构
   document.body.innerHTML = `
         <div class="yes-screen">
@@ -75,4 +76,7 @@ yesButton.addEventListener("click", function () {
 
   // 禁止滚动，保持页面美观
   document.body.style.overflow = "hidden";
-});
+};
+
+yesButton.addEventListener("click", ok);
+eggButton.addEventListener("click", ok);
